45052a804c9c86dd97605a16c18cc7ed3149b44a
赵俊浩 <zhaojunhao02@corp.netease.com>
2022-04-28 21:08:06 +0800

    organized the project structure

--- include/Chimes/Optimization/line_search.h
+++ include/Chimes/Optimization/line_search.h
@@ -1,14 +1,22 @@
 #pragma once
-
+#include <Eigen/Core>
 namespace Chimes
 {
     template <class Scalar>
     class LineSearchMethod
     {
     public:
+        template <class Fun>
+        LineSearchMethod(Fun& fun, Eigen::Matrix<Scalar, Eigen::Dynamic, 1>& init_x)
+        {
+                        
+        }
         virtual void solve() = 0;
     protected:
         template <class Fun>
-        Scalar lineSearch(Fun& f, Scalar e);
+        Scalar lineSearch(Fun& fun, Scalar e)
+        {
+            return Scalar(0);
+        }
     };
 } // namespace Chimes
---
+++ src/Optimization/line_search.cpp
@@ -0,0 +1,6 @@
+#include "Chimes/Optimization/line_search.h"
+
+namespace Chimes
+{
+
+} // namespace Chimes
